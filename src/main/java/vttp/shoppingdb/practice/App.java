160430afa309ef37945a1f66ec3621c4ff3c2a6c
drewkwan <andrewtimkwan@gmail.com>
2022-06-15 16:02:18 +0800

    first commit

---
+++ src/main/java/vttp/shoppingdb/practice/App.java
@@ -0,0 +1,13 @@
+package vttp.shoppingdb.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
